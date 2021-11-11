2bc6a61c3fca13bc05e9cc49c6499594f618b0a3
Carlos Lima <tec.carlos.lima@gmail.com>
2021-11-11 15:47:48 -0300

    layout com blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,28 +1,18 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.main')
 
-        <title>Laravel</title>
+@section('title', 'HDC Events') 
 
-        <link rel="stylesheet" href="/css/styles.css">
+@section('content')
+    <img src="/img/banner.jpg" alt="" srcset="">
+    <h1>{{$nome}}</h1>
+    @for ($i = 0; $i < count($arr); $i++)
+        <p>{{$arr[$i]}}</p>
+    @endfor
+    <!-- Comentario com o Blade -->
+    {{-- Teste do comentatio--}}
 
-        <script src="/js/scripts.js"></script>
-       
-    </head>
-    <body >
-        <img src="/img/banner.jpg" alt="" srcset="">
-        <h1>{{$nome}}</h1>
-        @for ($i = 0; $i < count($arr); $i++)
-            <p>{{$arr[$i]}}</p>
-        @endfor
-        <!-- Comentario com o Blade -->
-        {{-- Teste do comentatio--}}
-
-        @foreach ($nomes as $item)
-            <p>index do loop: {{$loop->index}}</p>
-            <p>{{$item}}</p>
-        @endforeach
-    </body>
-</html>
+    @foreach ($nomes as $item)
+        <p>index do loop: {{$loop->index}}</p>
+        <p>{{$item}}</p>
+    @endforeach
+@endsection
