9c15ecf7546f1d2b58cdf1aa83465ce2c3f41ac3
Carlos Lima <tec.carlos.lima@gmail.com>
2021-11-12 13:59:01 -0300

    modificado a navegação

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -24,37 +24,44 @@
 
         <header>
             <nav class="navbar navbar-expand-lg navbar-light">
-                <div class="collapse navbar-collapse" id="navbar">
+                <div class="container-fluid">
                     <a href="/" class="navbar-brand">
-                        <img src="/img/hdcevents_logo.svg" alt="HDC Events">
+                        <img src="/img/hdcevents_logo.svg" alt="HDC Events" class="navbar-brand-img" width="30" height="24">
                     </a>
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a href="/" class="nav-link">Eventos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="/events/create" class="nav-link">Criar Eventos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="/" class="nav-link">Entrar</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="/" class="nav-link">Cadastrar</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="/contact" class="nav-link">Contato</a>
-                        </li>
-                    </ul>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a href="/" class="nav-link">Eventos</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="/events/create" class="nav-link">Criar Eventos</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="/" class="nav-link">Entrar</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="/" class="nav-link">Cadastrar</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="/contact" class="nav-link">Contato</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
+                
             </nav>
         </header>
 
         @yield('content')
 
-        <footer>
+        <footer class="mt-auto">
             <p>HDC Events &copy; 2021</p>
         </footer>
 
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
         <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
         <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
     </body>
