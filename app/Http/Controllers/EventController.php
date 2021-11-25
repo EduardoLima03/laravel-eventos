ef85e61bbfe1759bdb23e81f5c4ab3488aabe878
Carlos Lima <tec.carlos.lima@gmail.com>
2021-11-25 16:17:47 -0300

    26-removendo presenca

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -77,10 +77,24 @@ class EventController extends Controller
     public function show($id){
         $event = Event::findOrFail($id);
 
+        $user = auth()->user();
+        $hasUserJoined = false;
+
+        if($user){
+            $userEvents = $user->eventsAsParticipant->toArray();
+
+            foreach($userEvents as $userEvent){
+                if($userEvent['id'] == $id){
+                    $hasUserJoined = true;
+                }
+            }
+        }
+
+
         //criado do evento, pegango seus dados
         $eventOwner = User::where('id', $event->user_id)->first()->toArray();
 
-        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
+        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner, 'hasUserJoined' => $hasUserJoined]);
     }
 
     public function dashboard()
@@ -156,5 +170,16 @@ class EventController extends Controller
         $event = Event::findOrFail($id);
 
         return redirect('/dashboard')->with('msg', 'Sua presença está confirmada no evento ' . $event->title);
+    }
+
+    public function leaveEvent($id){
+        $user = auth()->user();
+
+        //removendo da lista
+        $user->eventsAsParticipant()->detach($id);
+
+        $event = Event::findOrFail($id);
+
+        return redirect('/dashboard')->with('msg', 'Você saiu com sucesso do evento: ' . $event->title);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,5 @@ Route::put('/events/update/{id}', [EventController::class, 'update'])->middlewar
 Route::get('/dashboard', [EventController::class, 'dashboard'])->middleware('auth');
 
 Route::get('/events/join/{id}', [EventController::class, 'joinEvent'])->middleware('auth');
+
+Route::delete('/events/leave/{id}', [EventController::class, 'leaveEvent'])->middleware('auth');
