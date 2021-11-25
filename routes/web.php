a829cfde50076d2775195ef77d70feb36069b07c
Carlos Lima <tec.carlos.lima@gmail.com>
2021-11-25 09:29:29 -0300

    26-deletando enveto

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create'])->middleware('auth');
 Route::post('/events', [EventController::class, 'store']);
 Route::get('/events/{id}', [EventController::class, 'show']);
+Route::delete('/events/{id}', [EventController::class, 'destroy']);
 
 Route::get('/dashboard', [EventController::class, 'dashboard'])->middleware('auth');
 
